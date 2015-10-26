1d8fd1d383596dc29097614323dff3e90ab975a2
Steve Sun <jiahuan@amazon.com>
2015-10-25 21:34:55 -0700

    added .classpath

---
+++ src/com/tutorialspoint/demoImportAnnotation/App2Config.java
@@ -0,0 +1,28 @@
+package com.tutorialspoint.demoImportAnnotation;
+
+/** This is a class used for MainApp to demo @Import annotation.*/
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.tutorialspoint.demoCommon.TextEditor;
+
+@Configuration
+@Import(App1Config.class)
+public class App2Config {
+
+    @Bean
+    public App1 app1() {
+        return new App1();
+    }
+
+    @Bean
+    public App2 app2() {
+        return new App2();
+    }
+
+    @Bean(initMethod = "init", destroyMethod = "cleanup")
+    public TextEditor textEditor() {
+        return new TextEditor(null);
+    }
+}
